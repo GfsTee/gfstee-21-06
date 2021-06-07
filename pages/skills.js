cee7ec294280edb2e0f976473ad6d78c63904703
Georg Treitz <georg.treitz@gmail.com>
2021-06-07 21:37:48 +0200

    skills started

--- pages/skills.js
+++ pages/skills.js
@@ -1,8 +1,25 @@
+import skillData from '../data/skills.json'
+
 const Skills = () => {
     return (
         <>
             <main>
                 <h1>My Skills</h1>
+
+                <div>
+                    {skillData.map(skill => <figure>
+                        <img src={skill.skillIcon} alt={skill.skillName} />
+                        <figcaption>
+                            <span
+                                style={{
+                                    width: `${skill.skill}%`,
+                                    background: `${skill.skillColor}`,
+                                    color: `black`
+                                }}
+                            >{skill.skill}%</span>
+                        </figcaption>
+                    </figure>)}
+                </div>
             </main>
             <style jsx>{`
             main {
@@ -11,6 +28,18 @@ const Skills = () => {
                 flex-direction: column;
                 justify-content: center;
                 align-items: center
+            }
+            figure {
+                display: flex;
+            }
+            figcaption {
+                width: 300px
+            }
+            figcaption span {
+                display: block;
+                transition: width 1s;
+                font-size: .8rem;
+                text-align: center;
             }
             `}</style>
         </>
